bd45f6cb2f57f3d3939db1d78f484a19e8e0a66c
MartinMurithi <wachiramartin84@gmail.com>
2023-12-15 08:06:15 +0300

    ..

---
+++ index.js
@@ -0,0 +1,43 @@
+const scrollContainer = document.getElementById("scrollContainer");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+const menuIcon = document.getElementById("menuIcon");
+const xIcon = document.getElementById("xIcon");
+const mobileNav = document.getElementById("mobileNavListContainer");
+
+// scrollContainer.addEventListener('wheel', (e) => {
+//     e.preventDefault();
+//     scrollContainer.style.scrollBehavior = 'smooth';
+//     scrollContainer.scrollLeft += e.deltaY;
+// });
+
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 900;
+});
+
+prevBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 900;
+});
+
+menuIcon.addEventListener("click", () => {
+  mobileNav.style.display = "block";
+  menuIcon.style.display = "none";
+  xIcon.style.display = "block";
+
+  setTimeout(() => {
+    mobileNav.style.opacity = "1";
+  }, 10);
+});
+
+xIcon.addEventListener("click", () => {
+  mobileNav.style.opacity = "0";
+
+  setTimeout(() => {
+    mobileNav.style.display = "none";
+    xIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  }, 300);
+});
